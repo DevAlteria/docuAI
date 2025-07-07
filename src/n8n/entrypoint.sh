b3fd72817e9622fc9b636f47a14a1202a0c8893d
devAlteria <dev@alteriaautomation.com>
2025-07-07 18:09:53 +0200

    autoprovisiong user n8n

--- src/n8n/entrypoint.sh
+++ src/n8n/entrypoint.sh
@@ -8,9 +8,28 @@ if [ -f /home/node/.n8n/.prosioned ]; then
   exit 0
 fi
 
-echo IMPORTING CCREDENTIALS
+echo IMPORTING CREDENTIALS
 n8n import:credentials --separate --input=/credentials
+
+echo IMPORTING WORKFLOWS
 n8n import:workflow --separate --input=/workflows
+
+echo CREATING DEFAULT ACCOUNT
+echo -n Waiting for n8n
+n8n start > /tmp/startupn8n 2>&1 &
+MA_VAR=$(cat /tmp/startupn8n | grep "Editor is now accessible via:" | wc -l)
+while [ "$MA_VAR" != "1" ];
+do 
+  sleep 1
+  MA_VAR=$(cat /tmp/startupn8n | grep "Editor is now accessible via:" | wc -l)
+  echo -n .
+done
+echo 
+curl -X POST http://iadoc.alteria.vpn.alonsom.com/n8n/rest/owner/setup -H "Content-Type: application/json" -d '{"email":"dev@alteriaautomation.com","firstName":"alteria","lastName":"dev","password":"AlteriaKillM3!"}'
+kill -9 $(ps -a | grep "n8n start" | awk '{ print $1 }' | head -n1)
 
 echo PROSIONED
 touch /home/node/.n8n/.prosioned
+
+echo STARTING
+n8n start
